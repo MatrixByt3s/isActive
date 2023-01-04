297a9da16efd945942e931787918b6685520cc94
Omni <119423081+0x1-omnidev@users.noreply.github.com>
2023-01-04 17:58:18 +0100

    Update github.py

--- github.py
+++ github.py
@@ -1,38 +1,20 @@
-import base64
-import requests
-import json
+from github import Github
+from sys import argv
 
-state = "online"
+state = argv[-1]
+
 if state == "online":
     msg = "🟩 Active!"
 else:
     msg = "🟥 Unactive!"
 
-name, repo = "0x1-omnidev", "0x1-omnidev"
+g = Github("//TOKEN//")
 
-base_url = f"https://api.github.com/repos/{name}/{repo}"
+file = "README.MD"
 
-token = ""
+username = "0x1-omnidev"
 
-headers = {
-    "accept": "application/vnd.github+json",
-    "authorization": "Bearer " + token,
-}
+repo = g.get_repo(f"{username}/{username}")
+contents = repo.get_contents(file)
 
-def get_sha(file_path):
-    result = requests.get(f"{base_url}/contents/{file_path}",
-        headers = headers
-    )
-    sha = json.loads(result.text)["sha"]
-    return sha
-
-with open('README.md', 'r', encoding="utf-8") as file:
-    result = requests.put(f"{base_url}/contents/README.md",
-        headers = headers,
-
-        json = {
-            "message": "Update activity status",
-            "content": base64.b64encode(file.read().replace("[ACTIVITY]", msg).encode("utf-8")).decode("ascii"),
-            "sha": get_sha("README.md"),
-        }
-    )
+repo.update_file(contents.path, "update activity status", contents.decoded_content.decode().replace('[ACTIVITY]', msg), contents.sha)
